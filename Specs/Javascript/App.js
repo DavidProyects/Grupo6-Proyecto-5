ced07303aaf4e1c2fee7d03b66732e1fe11e7c68
Dalouzeta <97847375+Dalouzeta@users.noreply.github.com>
2022-01-29 19:05:10 -0500

    specs

---
+++ Specs/Javascript/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/styles.css";
+import Character from "./Character";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Character/>
+    </div>
+  );
+}
